8f86bba7bbbe8dc085b5bc2ede3bacae80c77b95
paivedant0906 <74344625+paivedant0906@users.noreply.github.com>
2021-02-03 18:06:53 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,36 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  }
+
+  display(){
+    image(bi,displayWidth/2-680,displayHeight/2-380,displayWidth,displayHeight-100);
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(displayWidth/2-50, 10);
+
+    this.input.position(displayWidth/2-50, displayHeight/2-100);
+    this.button.position(displayWidth/2+20, displayHeight/2-30);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2-30, displayHeight/2-100);
+    });
+
+  }
+}
